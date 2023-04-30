191d639e6c3f3c7a0cc003662ee255b7503638f7
joachimschmidt557 <joachim.schmidt557@outlook.com>
2023-04-30 20:41:54 +0200

    Update to latest zig
    zig version 0.11.0-dev.2862+66da64d77

--- build.zig
+++ build.zig
@@ -29,10 +29,13 @@ pub fn build(b: *Builder) void {
         exe.linkSystemLibrary("libsystemd");
     }
 
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
